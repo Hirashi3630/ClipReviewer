cbe25cdc7cb925d122fbbac1fa7d5773add16a32
Hirashi3630 <37778278+Hirashi3630@users.noreply.github.com>
2023-02-09 16:12:43 +0100

    added resources

--- ClipReviewer/Properties/Resources.Designer.cs
+++ ClipReviewer/Properties/Resources.Designer.cs
@@ -59,5 +59,14 @@ namespace ClipReviewer.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to https://github.com/Hirashi3630/ClipReviewer.
+        /// </summary>
+        internal static string GithubURL {
+            get {
+                return ResourceManager.GetString("GithubURL", resourceCulture);
+            }
+        }
     }
 }
